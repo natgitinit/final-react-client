480e0960abe976b691b94ee944501c4b4f97a83d
Natasha Gitlin <natasha.gitlin8@gmail.com>
2017-11-15 23:49:48 -0500

    app.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,3 @@
-// import React, { Component } from 'react';
-// //add imports
-// import {
-//   BrowserRouter as Router,
-//   Route
-// } from 'react-router-dom';
-// import NavBar from './components/NavBar';
-// import Home from './components/Home';
-// import Categories from './components/Categories';
-// import About from './components/About';
-// import NewsArticles from './components/NewsArticles';
-// import LatestNews from './containers/LatestNews';
-// import SearchNews from './containers/SearchNews';
-//
-// export class App extends Component {
-//   render() {
-//     return (
-//       <Router>
-//         <div className="app">
-//         <div className="navbar">
-//             <NavBar />
-//          </div>
-//          <Route exact path="/" component={Home} />
-//          <Route exact path="/about" component={About} />
-//          <Route exact path="/articleegories" component={Categories} />
-//          <NewsArticles />
-//          <SearchNews />
-//         </div>
-//       </Router>
-//     );
-//   }
-// }
-//
-// export default App;
-
 import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
